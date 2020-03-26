b9e5746d4951bbd85fb6aa6dfe8b85c51389bbc6
Pipapa <yezhengmaolove@gmail.com>
2020-03-26 15:50:20 +0800

    [feat] 添加 node 成员变量, 执行线程

--- node/node.go
+++ node/node.go
@@ -12,11 +12,12 @@ type Node struct {
 	server *server.HttpServer
 
 	id       message.Identify
-	view     message.Identify
+	view     message.View
 	table    map[message.Identify]string
 	faultNum uint
 
 	lastReply      *message.LastReply
+	sequence       *Sequence
 	executeNum     *ExecuteOpNum
 
 	buffer         *message.Buffer
@@ -26,7 +27,8 @@ type Node struct {
 	prepareRecv    chan *message.Prepare
 	commit         chan *message.Commit
 
-	prePrepareNotify chan bool
+	prePrepareSendNotify chan bool
+	executeNotify        chan bool
 }
 
 func NewNode(cfg *cmd.SharedConfig) *Node {
@@ -41,7 +43,8 @@ func NewNode(cfg *cmd.SharedConfig) *Node {
 		table:	  cfg.Table,
 		faultNum: cfg.FaultNum,
 		// lastReply state
-		lastReply: message.NewLastReply(),
+		lastReply:  message.NewLastReply(),
+		sequence:   NewSequence(cfg),
 		executeNum: NewExecuteOpNum(),
 		// the message buffer to store msg
 		buffer: message.NewBuffer(),
@@ -50,10 +53,12 @@ func NewNode(cfg *cmd.SharedConfig) *Node {
 		prePrepareRecv: make(chan *message.PrePrepare),
 		prepareRecv:    make(chan *message.Prepare),
 		commit:         make(chan *message.Commit),
-		// chan for notify thread
-		prePrepareNotify: make(chan bool),
+		// chan for notify pre-prepare send thread
+		prePrepareSendNotify: make(chan bool),
+		// chan for notify execute op and reply thread
+		executeNotify:        make(chan bool),
 	}
-	log.Printf("[Node] the node id : %d, view : %d, fault number: %d\n", node.id, node.view, node.faultNum)
+	log.Printf("[Node] the node id:%d, view:%d, fault number:%d\n", node.id, node.view, node.faultNum)
 	return node
 }
 
@@ -61,5 +66,10 @@ func (n *Node) Run() {
 	// first register chan for server
 	n.server.RegisterChan(n.requestRecv, n.prePrepareRecv, n.prepareRecv, n.commit)
 	go n.server.Run()
-	go n.RequestRecvThread()
+	go n.requestRecvThread()
+	go n.prePrepareSendThread()
+	go n.prePrepareRecvAndPrepareSendThread()
+	go n.prepareRecvAndCommitSendThread()
+	go n.commitRecvThread()
+	go n.executeAndReplyThread()
 }
